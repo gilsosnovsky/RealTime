3a9701fbc5c338189cc5ffc15e014e65033bb993
royiluz <royi.iluz@gmail.com>
2020-05-21 10:31:18 +0300

    Organize Code

--- real_time/src/comps/MyPage.js
+++ real_time/src/comps/MyPage.js
@@ -1,36 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NavBar from './NavBar/NavBar';
 import PageBody from './PageBody/PageBody';
 import Footer from './Footer/Footer';
 
-class MyPage extends Component{
-    state={
+class MyPage extends Component {
+    state = {
         NavBarState: 'welcomePage',
         PageBodyState: 'welcomePage',
         FooterState: 'welcomePage'
     }
 
-    onClickToEmployees= ()=> {
-        this.setState ( {
+    onClickToEmployees = () => {
+        this.setState({
             PageBodyState: 'toEmployees'
-        } )
+        })
     }
 
-    onClickLogo= ()=> {
-        this.setState ( {
+    onClickLogo = () => {
+        this.setState({
             PageBodyState: 'welcomePage'
-        } )
+        })
     }
 
 
-    render(){
-        return(
-            <div id = "MyPage">
-                <NavBar 
-                clickToEmployees={this.onClickToEmployees}
-                clickOnLogo={this.onClickLogo} />
-                <PageBody pageBodystate={this.state.PageBodyState}/>
-                <Footer/>
+    render() {
+        return (
+            <div id="MyPage">
+                <NavBar
+                    clickToEmployees={this.onClickToEmployees}
+                    clickOnLogo={this.onClickLogo} />
+                <PageBody pageBodystate={this.state.PageBodyState} />
+                <Footer />
             </div>
         )
     }
--- real_time/src/comps/NavBar/NavBar.js
+++ real_time/src/comps/NavBar/NavBar.js
@@ -2,30 +2,30 @@ import React from "react";
 import logo from "./logo.gif";
 import "./NavBar.css";
 
-const NavBar =(props) => {
+const NavBar = (props) => {
 
-    return (
-      <div id="NavBar">
-        <div id="NavBarButtons">
-          <label id="contact" className="NavBarButton">
-            צור קשר
+  return (
+    <div id="NavBar">
+      <div id="NavBarButtons">
+        <label id="contact" className="NavBarButton">
+          צור קשר
           </label>
-          <label id="about_us" className="NavBarButton">
-            מי אנחנו
+        <label id="about_us" className="NavBarButton">
+          מי אנחנו
           </label>
-          <label id="to_business" className="NavBarButton">
-            לעסקים
+        <label id="to_business" className="NavBarButton">
+          לעסקים
           </label>
-          <label
-            id="to_employees"
-            className="NavBarButton"
-            onClick={props.clickToEmployees}
-          >
-            לעובדים
+        <label
+          id="to_employees"
+          className="NavBarButton"
+          onClick={props.clickToEmployees}
+        >
+          לעובדים
           </label>
-          <img id="logo" src={logo} alt="Logo" onClick={props.clickOnLogo}/>
-        </div>
+        <img id="logo" src={logo} alt="Logo" onClick={props.clickOnLogo} />
       </div>
-    );  
+    </div>
+  );
 }
 export default NavBar;
