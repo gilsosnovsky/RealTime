608f1c89e6db0440f39a66442d11556fed9d8956
Sahar Malka <saharmalka@gmail.com>
2020-05-21 14:25:31 +0300

    Business form added

--- real_time/src/comps/NavBar/NavBar.js
+++ real_time/src/comps/NavBar/NavBar.js
@@ -3,33 +3,36 @@ import logo from "./logo.gif";
 import "./NavBar.css";
 
 const NavBar = (props) => {
-
   return (
     <div id="NavBar">
       <div id="NavBarButtons">
-        <label id="contact"
-         className="NavBarButton"
-         onClick={props.clickContact}>
+        <label
+          id="ContactUs"
+          className="NavBarButton"
+          onClick={props.clickContact}
+        >
           צור קשר
-          </label>
+        </label>
         <label id="about_us" className="NavBarButton">
           מי אנחנו
-          </label>
-          <label id="to_business"
-           className="NavBarButton"
-           onClick={props.clickToBusiness} >
-            לעסקים
-          </label>
+        </label>
+        <label
+          id="to_business"
+          className="NavBarButton"
+          onClick={props.clickToBusiness}
+        >
+          לעסקים
+        </label>
         <label
           id="to_employees"
           className="NavBarButton"
           onClick={props.clickToEmployees}
         >
           לעובדים
-          </label>
+        </label>
         <img id="logo" src={logo} alt="Logo" onClick={props.clickOnLogo} />
       </div>
     </div>
   );
-}
+};
 export default NavBar;
--- real_time/src/comps/WelcomePage/ToBusiness.js
+++ real_time/src/comps/WelcomePage/ToBusiness.js
@@ -1,54 +1,98 @@
-import React from 'react';
-import './ToBusiness.css'
-
-
-
+import React from "react";
+import "./ToBusiness.css";
 
 const ToBusiness = (props) => {
-    return (
-        <div id="toBusiness">
-            <label id="headLine">
-            זמן אמת הפתרון המושלם לבעיית כוח האדם בעקבות משבר הקורונה!
-            </label>
-            <label id="paragraph_1">
-            זמן אמת היא פלטפורמה ירושלמית שמתמחה בגיוס, שיבוץ, וניהול עובדים יומיים.
-            <br/>
-             מטרתנו היא לעזור למעסיקים בירושלים למצוא עובדים/ות לעבודות מזדמנות וקצרות טווח.
-             <br/>
-              באמצעותנו ספקים שונים יכולים להתמודד עם התנודתיות בעקבות המשבר, שיוצר קשיים יומיומיים בניהול כוח אדם. 
-            <br/>
-              בזכות הטכנולוגיה שפיתחנו, בשילוב עם רשת קשרים מקצועית ברחבי העיר, ביכולתנו לספק עובדים ליום או יותר,
-            <br/>
-               בכל רחבי העיר, בהזמנה מראש או בהתראה קצרה, למגוון רחב של תפקידים ומקצועות. 
-            </label>
-            <label id="intrestMe">
-                <label id="headLineIntrest">
-                !מעניין אותי
-                <br/>
-                אשמח לשמוע פרטים
-                </label>
-                <div id="form">
-                    <label for="fname">שם:</label>
-                    <input type="text" id="fname" name="fname"/><br></br>
-                    <label for="fphone">טלפון:</label>
-                    <input type="tel" id="phoneNumber" name="fphone"/><br></br>
-                    <label for="femail">אימייל:</label>
-                    <input type="email" id="femail" name="femail"/><br></br>
-                    <label for="fhowYouGotUs">איך הגעת אלינו ?</label>
-                    <input type="text" id="fhowYouGotUs" name="fhowYouGotUs"/><br></br>
-                    <label for="fbusinessName"> שם העסק:</label>
-                    <input type="text" id="fbusinessName" name="fbusinessName"/><br></br>
-                    <label for="ftypeOfjob">המשרות המוצעות:</label>
-                    <input type="radio" id="shortTerm" name="shortTerm" value="shortTerm"/>
-                    <label for="shortTerm">טווח קצר</label><br/>
-                    <input type="radio" id="longTerm" name="longTerm" value="longTerm"/>
-                    <label for="longTerm">עבודה קבועה</label><br/>
-                    <input type="radio" id="other" name="gender" value="other"/>
-                    <label for="other">גם וגם</label>
-                </div>
-            </label>
+  return (
+    <div id="toBusiness">
+      <div id="info">
+        <label id="headLine">
+          זמן אמת הפתרון המושלם לבעיית כוח האדם בעקבות משבר הקורונה!
+        </label>
+        <label id="paragraph_1">
+          זמן אמת היא פלטפורמה ירושלמית שמתמחה בגיוס, שיבוץ, וניהול עובדים
+          יומיים.
+          <br />
+          מטרתנו היא לעזור למעסיקים בירושלים למצוא עובדים/ות לעבודות מזדמנות
+          וקצרות טווח.
+          <br />
+          באמצעותנו ספקים שונים יכולים להתמודד עם התנודתיות בעקבות המשבר, שיוצר
+          קשיים יומיומיים בניהול כוח אדם.
+          <br />
+          בזכות הטכנולוגיה שפיתחנו, בשילוב עם רשת קשרים מקצועית ברחבי העיר,
+          ביכולתנו לספק עובדים ליום או יותר,
+          <br />
+          בכל רחבי העיר, בהזמנה מראש או בהתראה קצרה, למגוון רחב של תפקידים
+          ומקצועות.
+        </label>
+      </div>
+      <div id="intrestMe">
+        <div class="container">
+          <form id="contact" action="" method="post">
+            <h3>!מעניין אותי</h3>
+            <h4>אשמח לשמוע פרטים</h4>
+            <fieldset>
+              <input
+                id="fname"
+                placeholder="שם:"
+                type="text"
+                tabindex="1"
+                required
+                autofocus
+              />
+            </fieldset>
 
+            <fieldset>
+              <input placeholder="אימייל:" type="email" tabindex="2" required />
+            </fieldset>
+
+            <fieldset>
+              <input placeholder="טלפון:" type="tel" tabindex="3" required />
+            </fieldset>
+
+            <fieldset>
+              <input
+                id="fname"
+                placeholder="איך הגעת אלינו?"
+                type="text"
+                tabindex="4"
+                required
+                autofocus
+              />
+            </fieldset>
+
+            <fieldset>
+              <input
+                id="fname"
+                placeholder="שם העסק:"
+                type="text"
+                tabindex="5"
+                required
+                autofocus
+              />
+            </fieldset>
+
+            <fieldset>
+              <textarea
+                placeholder="עוד משהו להוסיף?"
+                tabindex="5"
+                required
+              ></textarea>
+            </fieldset>
+
+            <fieldset>
+              <button
+                name="submit"
+                type="submit"
+                id="contact-submit"
+                data-submit="...Sending"
+              >
+                Submit
+              </button>
+            </fieldset>
+          </form>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default ToBusiness;
