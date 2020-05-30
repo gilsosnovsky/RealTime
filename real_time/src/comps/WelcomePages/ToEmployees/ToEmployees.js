a75c5da983f994db4b4059723916ce01c8631088
michaelba8 <63546875+michaelba8@users.noreply.github.com>
2020-05-30 19:57:06 +0300

    fixes-michael

--- real_time/src/comps/WelcomePages/ToEmployees/ToEmployees.js
+++ real_time/src/comps/WelcomePages/ToEmployees/ToEmployees.js
@@ -6,18 +6,26 @@ import Carousel from 'react-bootstrap/Carousel';
 const toEmployees = (props) => {
     return (
         <div id="toEmployees">
-            <div id="employees_p1">
-            מחפשים עבודה? בשביל זה אנחנו כאן!
-            </div>
-            <br />
-            <div id="employees_p2">
-            בזמן אמת, ביכולתנו לסייע לנוער וצעירים/ות המחפשים הכנסה
-             <br />
-             כספית על בסיס יומי דרך הצעות עבודה אטרקטיביות.
-             <br />
-             נכנסנים לאתר ומקבלים הצעות עבודה יומיות וגמישות ללא
-              <br />
-              מחויבות לכמות משמרות במגוון מקצועות וענפים ברחבי ירושלים.
+            <div id='employees_top'>
+                <div id ="employees_top_right">
+                    <img id='employees_phone_pic' src = {require('./phone_pic.jpg')} />  
+                </div>
+                <div id="employees_p1">
+                מחפשים עבודה? בשביל זה אנחנו כאן!
+                <br />
+                </div>
+                <div id="employees_p2">
+                        בזמן אמת, ביכולתנו לסייע לנוער וצעירים/ות המחפשים הכנסה
+                        <br />
+                        כספית על בסיס יומי דרך הצעות עבודה אטרקטיביות.
+                        <br />
+                        נכנסנים לאתר ומקבלים הצעות עבודה יומיות וגמישות ללא
+                        <br />
+                        מחויבות לכמות משמרות במגוון מקצועות וענפים ברחבי ירושלים.
+                </div>
+                <div id ="employees_top_left">
+                    <img id='employees_waiter_pic' src = {require('./waiter_pic.jpg')} />
+                </div>
             </div>
             <p></p>
             <div id="carousel_container">
@@ -57,15 +65,23 @@ const toEmployees = (props) => {
             </div>
             <p />
             <div id="employees_p3">
-            אנחנו ב'זמן אמת' בחרנו בשבילכם/ן את העסקים הכי טובים בירושלים.
-                <br />
-                אתם רק צריכים להחליט מתי, איפה וכמה בא לכם/ן לעבוד! 
-                <br />
-                אנחנו מודעים לקשיים שנוצרו במציאת עבודה בעקבות משבר הקורונה. 
-                <br />
-                לכן, בזמן אמת החלטנו לפתח טכנולוגיה שתעזור לכם למצוא עבודה 
-                <br />
-                מכל מקום ובכל זמן בקליק אחד!
+                <div id= 'employees_p3_empty'>
+                    <img id='employees_hands_pic' src = {require('./employees_hands.jpg')} />   
+                </div>
+                <div id='employees_p3_text'>
+                    אנחנו ב'זמן אמת' בחרנו בשבילכם/ן את העסקים הכי טובים בירושלים.
+                        <br />
+                        אתם רק צריכים להחליט מתי, איפה וכמה בא לכם/ן לעבוד! 
+                        <br />
+                        אנחנו מודעים לקשיים שנוצרו במציאת עבודה בעקבות משבר הקורונה. 
+                        <br />
+                        לכן, בזמן אמת החלטנו לפתח טכנולוגיה שתעזור לכם למצוא עבודה 
+                        <br />
+                        מכל מקום ובכל זמן בקליק אחד!
+                </div>
+                <div id='employees_p3_pic'>
+                   
+                </div>
             </div>
 
             <div id="employees_p4">
