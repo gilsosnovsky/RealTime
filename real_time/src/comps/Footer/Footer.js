c8737422c650b4318abcc72062d21fd3ff1cde62
unknown <orelii626@gmail.com>
2020-05-31 11:48:47 +0300

    orel put links to instegram and facebook

--- real_time/src/comps/Footer/Footer.js
+++ real_time/src/comps/Footer/Footer.js
@@ -7,8 +7,12 @@ class Footer extends Component{
     render(){
         return(
             <div id = "footer">
+                <a href="https://www.facebook.com/זמן-אמת-Zman-Emet-101856524893149/" target="_blank">
                 <img id = "facebook_icon" className = "social_media_buttons" src = {facebook_icon} alt = "facebook"/>
+                </a>
+                <a href="https://www.instagram.com/zmanemet_jobs/" target="_blank">
                 <img id = "instagram_icon" className = "social_media_buttons" src = {instagram_icon} alt = "instagram"/>
+                </a>
                 <p/>
                 <label>ניתן להשיג אותנו בטלפון – 02-5456292 או במייל – Amit_le@jerusalem.muni.il</label>
             </div>
