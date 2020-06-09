fcb135278e7e2fc834abc6c927040d37d612a13e
royiluz <royi.iluz@gmail.com>
2020-06-09 18:13:11 +0300

    Royi - Employee Jobs

--- real_time/src/comps/EmployeePages/EmployeeJobOffers/EmployeeJobOffers.js
+++ real_time/src/comps/EmployeePages/EmployeeJobOffers/EmployeeJobOffers.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, { Component } from "react";
 import "./EmployeeJobOffers.css";
+import SingleJobItem from "./SingleJobItem";
 
-const EmployeeJobOffers = (props) => {
+class EmployeeJobOffers extends React.Component {
+  
+  state = {
+    jobs_list: [
 
+      {
+        type: 'מלצר',
+        hours: "08:00 - 17:00",
+        date: "28.07.2020",
+        place: "אולם אירועים  מלון יהודה, ירושלים",
+        salary: "420",
+        long_info: "מידע ארוך על משרת המלצר",
+        logo: "https://grupohonor.com.br/wp-content/uploads/2015/10/Logo-Default.png"
+      },
+
+      {
+        type: 'טבח',
+        hours: "08:30 - 16:00",
+        date: "28.06.2020",
+        place: "מלון דניאל, ים המלח",
+        salary: "350",
+        long_info: "מידע ארוך על משרת הטבח",
+        logo: "https://images.globes.co.il/Images/NewGlobes/big_image/2014/aroma-575.2014731T130210.jpg"
+      },
+
+      {
+        type: 'נהג',
+        hours: "18:00 - 00:00",
+        date: "03.07.2020",
+        place: "תחנה מרכזית, ירושלים",
+        salary: "500",
+        long_info: "מידע ארוך על משרת הנהג",
+        logo: "https://files.geektime.co.il/wp-content/uploads/2018/06/gett-1528377628.jpg"
+      }
+    ]
+  }
+  render() {
     return (
       <div id="jobs_employee">
-          Jobs
+        משרות במיוחד עבורך
+        <ul>
+          {
+            this.state.jobs_list.map((job, index) => {
+              return <SingleJobItem type={job.type}
+                hours={job.hours}
+                date={job.date}
+                place={job.place}
+                salary={job.salary}
+                long_info={job.long_info}
+                logo={job.logo} />
+            })
+          }
+        </ul>
       </div>
     );
- 
-};
+  }
+}
 export default EmployeeJobOffers;
---
+++ real_time/src/comps/EmployeePages/EmployeeJobOffers/SingleJobItem.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react";
+import $ from "jquery";
+import './SingleJobItem.css'
+
+class SingleJobItem extends React.Component {
+    constructor(props) {
+        super(props);
+        this.openFullInfo = this.openFullInfo.bind(this);
+        this.hideFullInfo = this.hideFullInfo.bind(this);
+    }
+    state = {
+        job_type: this.props.type,
+        job_hours: this.props.hours,
+        job_date:  this.props.date,
+        job_place:  this.props.place,
+        job_salary:  this.props.salary,
+        job_logo: this.props.logo,
+        job_long_info: this.props.long_info,
+        item_bottom_visibility: 'hidden',
+        container_height: '120px',
+        item_top_height: '100%',
+        item_bottom_height: '0%'
+    };
+    openFullInfo() {
+        this.setState({
+            item_bottom_visibility: 'visible',
+            container_height: '240px',
+            item_top_height: '50%',
+            item_bottom_height: '50%'
+        });
+    }
+    hideFullInfo() {
+        this.setState({
+            item_bottom_visibility: 'hidden',
+            container_height: '120px',
+            item_top_height: '100%',
+            item_bottom_height: '0%'
+        });
+    }
+    render() {
+        return (
+            <div id="job_item_container"
+                style={{ height: `${this.state.container_height}` }}>
+                <div id="job_item_top" onClick={this.openFullInfo}
+                    style={{ height: `${this.state.item_top_height}` }}>
+                    <div id="job_item_logo_container">
+                        <img id="job_item_logo" src={this.state.job_logo} />
+                    </div>
+                    <div id="job_item_brief_info">
+                        <div class="brief_info">
+                            <b> קצת על העבודה: </b>{this.state.job_type}<br />
+                            <b> מתי: </b>{this.state.job_date} , {this.state.job_hours}<br />
+                            <b> איפה: </b>{this.state.job_place}
+                        </div>
+                    </div>
+                    <div id="job_item_salary_container">
+                        <div id="job_item_salary">
+                            {this.state.job_salary}₪
+                        </div>
+                    </div>
+                </div>
+                <div id="job_item_bottom"
+                    style={{ height: `${this.state.item_bottom_height}`, visibility: `${this.state.item_bottom_visibility}` }}>
+                    <div id="job_item_full_info">
+                        <b>פרטים נוספים:</b><br />
+                        {this.state.job_long_info}
+                    </div>
+                    <div id="job_item_buttons">
+                        <div id="job_item_buttons_no" onClick={this.hideFullInfo}>
+                            לא מתאים לי
+                        </div>
+                        <div id="job_item_buttons_yes">
+                            מתאים לי בול!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default SingleJobItem;
