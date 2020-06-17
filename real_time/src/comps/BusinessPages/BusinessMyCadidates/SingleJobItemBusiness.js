c06783f7f2184da485f5b7383de58615a2ef1b76
Sahar Malka <saharmalka@gmail.com>
2020-06-17 23:47:56 +0300

    Bussines pages+navBar- mobile

--- real_time/src/comps/BusinessPages/BusinessMyCadidates/SingleJobItemBusiness.js
+++ real_time/src/comps/BusinessPages/BusinessMyCadidates/SingleJobItemBusiness.js
@@ -1,56 +1,70 @@
 import React from "react";
-import "./SingleJobItemBusiness.css"
-
+import "./SingleJobItemBusiness.css";
 
 class singleJobItemBusiness extends React.Component {
-    
-    state = {
-        job_type: this.props.type,
-        job_hours: this.props.hours,
-        job_date:  this.props.date,
-        job_place:  this.props.place,
-        job_salary:  this.props.salary,
-        job_logo: this.props.logo,
-        job_long_info: this.props.long_info,
-        job_remarks: this.props.remarks,
-        job_clothing: this.props.clothing,
-        job_payment_time:this.props.payment_time
-    };
+  state = {
+    job_type: this.props.type,
+    job_hours: this.props.hours,
+    job_date: this.props.date,
+    job_place: this.props.place,
+    job_salary: this.props.salary,
+    job_logo: this.props.logo,
+    job_long_info: this.props.long_info,
+    job_remarks: this.props.remarks,
+    job_clothing: this.props.clothing,
+    job_payment_time: this.props.payment_time,
+  };
 
-    
+  render() {
+    return (
+      <div id="job_item_business_container">
+        <div id="job_item_business">
+          <div id="job_item_logo_business_container">
+            <img
+              id="job_item_business_logo"
+              src={this.state.job_logo}
+              alt="job_logo"
+            />
+          </div>
 
-    render() {
-        
-        return (
-            <div id="job_item_business_container">
-                <div id="job_item_business" >
-                    <div id="job_item_logo_business_container">
-                        <img id="job_item_business_logo" src={this.state.job_logo} alt="job_logo"/>
-                    </div>
-
-                    <div id="job_item_info_business">
-                        <div class="full_info_business">
-                            <b> תחום כללי של העבודה: </b>{this.state.job_type}<br />
-                            <b> מתי: </b>{this.state.job_date} , {this.state.job_hours}<br />
-                            <b> איפה: </b>{this.state.job_place}<br/>
-                            <b> משכורת: </b>{this.state.job_salary}₪<br/>
-                            <b>פרטים נוספים:</b>{this.state.job_long_info}<br/>
-                            <b>ביגוד: </b>{this.state.job_clothing}<br/>
-                            <b>זמן תשלום : </b>{this.state.job_payment_time}<br/>
-                            <b>הערות: </b>{this.state.job_remarks}
-                        </div>
-                        <div id="job_item_buttons_business">
-                        <div id="job_item_buttons_show_candi" onClick={this.props.setBodyTypeState}>
-                            צפייה במועמדים
-                        </div>
-                        <div id="job_item_buttons_delete_job">
-                            מחק משרה
-                        </div>
-                    </div>
-                    </div>
-                </div>
+          <div id="job_item_info_business">
+            <div class="full_info_business">
+              <b> תחום כללי של העבודה: </b>
+              {this.state.job_type}
+              <br />
+              <b> מתי: </b>
+              {this.state.job_date} , {this.state.job_hours}
+              <br />
+              <b> איפה: </b>
+              {this.state.job_place}
+              <br />
+              <b> משכורת: </b>
+              {this.state.job_salary}₪<br />
+              <b>פרטים נוספים:</b>
+              {this.state.job_long_info}
+              <br />
+              <b>ביגוד: </b>
+              {this.state.job_clothing}
+              <br />
+              <b>זמן תשלום : </b>
+              {this.state.job_payment_time}
+              <br />
+              <b>הערות: </b>
+              {this.state.job_remarks}
             </div>
-        );
-    }    
+          </div>
+        </div>
+        <div id="job_item_buttons_business">
+          <div
+            id="job_item_buttons_show_candi"
+            onClick={this.props.setBodyTypeState}
+          >
+            צפייה במועמדים
+          </div>
+          <div id="job_item_buttons_delete_job">מחק משרה</div>
+        </div>
+      </div>
+    );
+  }
 }
 export default singleJobItemBusiness;
--- real_time/src/comps/BusinessPages/BusinessPostJob/BusinessPostJob.js
+++ real_time/src/comps/BusinessPages/BusinessPostJob/BusinessPostJob.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./BusinessPostJob.css";
 import fire from "../../../firebaseConfig";
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 class BusinessPostJob extends React.Component {
-
   constructor(props) {
     super(props);
     this.onSubmitjob = this.onSubmitjob.bind(this);
@@ -20,51 +19,53 @@ class BusinessPostJob extends React.Component {
     remarks: "",
     clothing: "",
     payment_time: "",
-    payment_method: ""
+    payment_method: "",
   };
 
   onSubmitjob = (e) => {
     e.preventDefault();
-    this.setState({
-      type: this.type.value,
-      hours: this.from_hour.value + "-" + this.to_hour.value,
-      date: this.date.value,
-      place: this.place.value,
-      salary: this.salary.value,
-      long_info: this.detailes.value,
-      remarks: this.remarks.value,
-      clothing: this.clothing.value
-    }, () => {
-      const db = fire.database();
-      db.ref("/jobs/jobs_list").push(this.state);
-    });
-  }
+    this.setState(
+      {
+        type: this.type.value,
+        hours: this.from_hour.value + "-" + this.to_hour.value,
+        date: this.date.value,
+        place: this.place.value,
+        salary: this.salary.value,
+        long_info: this.detailes.value,
+        remarks: this.remarks.value,
+        clothing: this.clothing.value,
+      },
+      () => {
+        const db = fire.database();
+        db.ref("/jobs/jobs_list").push(this.state);
+      }
+    );
+  };
 
   onPaymentMethodChanged = (e) => {
     this.setState({
-      payment_method: e.currentTarget.value
+      payment_method: e.currentTarget.value,
     });
-  }
+  };
 
   onPaymentTimeChanged = (e) => {
     this.setState({
-      payment_time: e.currentTarget.value
+      payment_time: e.currentTarget.value,
     });
-  }
-
+  };
 
   render() {
     return (
       <div class="post_job_business">
         <div id="postJobContainer">
           <form action="" method="post" onSubmit={this.onSubmitjob}>
-            <div id="postJobTitle">פרסם עבודה</div>
+            <div id="postJobTitle">פרסם משרה</div>
 
             <fieldset>
               <input
                 class="field"
                 id="Ptype"
-                ref={(c) => this.type = c}
+                ref={(c) => (this.type = c)}
                 placeholder="תחום כללי של העבודה"
                 type="text"
                 tabindex="1"
@@ -77,7 +78,7 @@ class BusinessPostJob extends React.Component {
               <input
                 class="field"
                 id="Pdetailes"
-                ref={(c) => this.detailes = c}
+                ref={(c) => (this.detailes = c)}
                 placeholder="פרטי העבודה"
                 type="text"
                 tabindex="4"
@@ -90,7 +91,7 @@ class BusinessPostJob extends React.Component {
               <input
                 id="Pdate"
                 class="field"
-                ref={(c) => this.date = c}
+                ref={(c) => (this.date = c)}
                 placeholder="date"
                 type="date"
                 tabindex="3"
@@ -102,7 +103,7 @@ class BusinessPostJob extends React.Component {
               <h3>משעה:</h3>
               <input
                 class="field"
-                ref={(c) => this.from_hour = c}
+                ref={(c) => (this.from_hour = c)}
                 placeholder="שעות העבודה"
                 type="time"
                 tabindex="1"
@@ -111,7 +112,7 @@ class BusinessPostJob extends React.Component {
               <h3>עד שעה:</h3>
               <input
                 class="field"
-                ref={(c) => this.to_hour = c}
+                ref={(c) => (this.to_hour = c)}
                 placeholder="שעות העבודה"
                 type="time"
                 tabindex="1"
@@ -123,7 +124,7 @@ class BusinessPostJob extends React.Component {
               <input
                 class="field"
                 id="Ppayment"
-                ref={(c) => this.salary = c}
+                ref={(c) => (this.salary = c)}
                 placeholder="שכר"
                 type="text"
                 tabindex="4"
@@ -135,13 +136,29 @@ class BusinessPostJob extends React.Component {
             <fieldset>
               <div class="radioButtons">
                 <p>אופן התשלום:</p>
-                <input type="radio" name="gender" value="מזומן" onChange={this.onPaymentMethodChanged} required/>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="מזומן"
+                  onChange={this.onPaymentMethodChanged}
+                  required
+                />
                 <label for="cash">מזומן</label>
                 <br />
-                <input type="radio" name="gender" value="המחאה" onChange={this.onPaymentMethodChanged} />
+                <input
+                  type="radio"
+                  name="gender"
+                  value="המחאה"
+                  onChange={this.onPaymentMethodChanged}
+                />
                 <label for="check">המחאה</label>
                 <br />
-                <input type="radio" name="gender" value="העברה בנקאית" onChange={this.onPaymentMethodChanged} />
+                <input
+                  type="radio"
+                  name="gender"
+                  value="העברה בנקאית"
+                  onChange={this.onPaymentMethodChanged}
+                />
                 <label for="transfer">העברה בנקאית</label>
               </div>
             </fieldset>
@@ -149,10 +166,21 @@ class BusinessPostJob extends React.Component {
             <fieldset>
               <div class="radioButtons" required>
                 <p>קבלת התשלום:</p>
-                <input type="radio" name="age" value="בסוף יום עבודה" onChange={this.onPaymentTimeChanged} required/>
+                <input
+                  type="radio"
+                  name="age"
+                  value="בסוף יום עבודה"
+                  onChange={this.onPaymentTimeChanged}
+                  required
+                />
                 <label for="age1">בסוף יום העבודה</label>
                 <br />
-                <input type="radio" name="age" value="בסוף החודש" onChange={this.onPaymentTimeChanged} />
+                <input
+                  type="radio"
+                  name="age"
+                  value="בסוף החודש"
+                  onChange={this.onPaymentTimeChanged}
+                />
                 <label for="age2">בסוף החודש</label>
                 <br />
               </div>
@@ -162,7 +190,7 @@ class BusinessPostJob extends React.Component {
               <input
                 class="field"
                 id="Pplace"
-                ref={(c) => this.place = c}
+                ref={(c) => (this.place = c)}
                 placeholder="מיקום העבודה"
                 type="text"
                 tabindex="4"
@@ -174,7 +202,7 @@ class BusinessPostJob extends React.Component {
             <fieldset>
               <input
                 class="field"
-                ref={(c) => this.business_name = c}
+                ref={(c) => (this.business_name = c)}
                 placeholder="שם העסק"
                 type="text"
                 tabindex="2"
@@ -185,7 +213,7 @@ class BusinessPostJob extends React.Component {
             <fieldset>
               <input
                 class="field"
-                ref={(c) => this.clothing = c}
+                ref={(c) => (this.clothing = c)}
                 placeholder="ביגוד מיוחד"
                 type="text"
                 tabindex="2"
@@ -196,7 +224,7 @@ class BusinessPostJob extends React.Component {
             <fieldset>
               <input
                 class="field"
-                ref={(c) => this.remarks = c}
+                ref={(c) => (this.remarks = c)}
                 placeholder="הערות"
                 type="text"
                 tabindex="2"
@@ -204,12 +232,19 @@ class BusinessPostJob extends React.Component {
               />
             </fieldset>
             <fieldset>
-              <button id="postButton" name="submit" data-submit="...Sending" type="submit">פרסם</button>
+              <button
+                id="postButton"
+                name="submit"
+                data-submit="...Sending"
+                type="submit"
+              >
+                פרסם
+              </button>
             </fieldset>
           </form>
         </div>
       </div>
     );
   }
-};
+}
 export default BusinessPostJob;
